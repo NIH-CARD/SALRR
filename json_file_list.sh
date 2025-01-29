cc1d932f7b09c1ad030b3ff9d60697f9e95df70a
Cedric Kouam <cedricdixi@gmail.com>
2025-01-29 16:13:11 -0500

    Added json files to file list

--- json_file_list.sh
+++ json_file_list.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-#script for finding the directory paths and the json files and creating a list of json files
-#The output  "json_report_paths.txt" will be used as the input  file for the longread-report-parser
+#script for finding the directory paths of the json files
 
-find /data/CARD_AUX/LRS_temp/NABEC_RNA/ -name "*.json" > /data/CARD_AUX/LRS_temp/NABEC_RNA/SEQ_REPORTS/json_report_paths.txt
+find /data/CARD_AUX/LRS_temp/NABEC_RNA/ -type -f  -name "*.json" > /data/CARD_AUX/LRS_temp/NABEC_RNA/SEQ_REPORTS/json_report_paths.txt
 
 
