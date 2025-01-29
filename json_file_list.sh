fb5dabb8651aec06c531f0fb9bdd48bc456fc9e8
Cedric Kouam <cedricdixi@gmail.com>
2025-01-29 16:26:03 -0500

    Correct of script -type f

--- json_file_list.sh
+++ json_file_list.sh
@@ -2,6 +2,6 @@
 
 #script for finding the directory paths of the json files
 
-find /data/CARD_AUX/LRS_temp/NABEC_RNA/ -type -f  -name "*.json" > /data/CARD_AUX/LRS_temp/NABEC_RNA/SEQ_REPORTS/json_report_paths.txt
+find /data/CARD_AUX/LRS_temp/NABEC_RNA/ -type f  -name "*.json" > /data/CARD_AUX/LRS_temp/NABEC_RNA/SEQ_REPORTS/json_report_paths.txt
 
 
