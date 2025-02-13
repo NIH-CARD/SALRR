ac8b2063757917bd40f882dea6c2bac2efed2bb4
Cedric Kouam <cedricdixi@gmail.com>
2025-02-13 08:13:38 -0500

    Update pychopper.sh

--- pychopper.sh
+++ pychopper.sh
@@ -16,30 +16,63 @@ SAMPLE_ID=$(sed -n ${N}p $SAMPLE_SHEET | cut -f 1)
 # second column is flow cell (e.g., PAY78456)
 FLOWCELL=$(sed -n ${N}p $SAMPLE_SHEET | cut -f 2)
 
+basecalling_model="cdna_r10.4.1_e8.2_400bps_sup@v5.0.0"
+
 # debugging output to slurm script
+
 echo "Job Array #${N}"
 echo "SAMPLE_ID ${SAMPLE_ID}"
 echo "FLOWCELL ${FLOWCELL}"
 
 # base directory paths
-#BASE_DIR=/data/CARD_AUX/LRS_temp/December_24/ # for example
+
 BASE_DIR="/data/CARD_AUX/LRS_temp/NABEC_RNA"
+ONT_BAM_DIR="${BASE_DIR}/ONT_UBAM/${SAMPLE_ID}"
+FASTQ_DIR="${BASE_DIR}/U_FASTQ/${SAMPLE_ID}"
+PYCHOPPER_DIR="${BASE_DIR}/PYCHOPPER/${SAMPLE_ID}"
+STATS_DIR="${PYCHOPPER_DIR}/stats"
+
 
 # make output directories and parent if necessary
-mkdir -p ${BASE_DIR}/U_FASTQ/${SAMPLE_ID}
-mkdir -p ${BASE_DIR}/PYCHOPPER/${SAMPLE_ID} #for pychopper output
-mkdir -p ${BASE_DIR}/PYCHOPPER/${SAMPLE_ID}/stats #for pychopper summary stats
+
+mkdir -p "${FASTQ_DIR}" "${PYCHOPPER_DIR}" "${STATS_DIR}"  
+
+
 
 # load modules
-#
+
 module load samtools/1.21
 module load pychopper/2.7.10
 
-# Trimming
-# debugging output with output path for unmapped BAM
-echo "${BASE_DIR}/U_FASTQ/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.trimmed.fastq"
-samtools fastq -T* -@ $SLURM_CPUS_PER_TASK -n ${BASE_DIR}/ONT_UBAM/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.bam > ${BASE_DIR}/U_FASTQ/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.fastq && \
-pychopper -t $SLURM_CPUS_PER_TASK -m phmm -k PCS114 -r ${BASE_DIR}/PYCHOPPER/${SAMPLE_ID}/stats/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.pdf -S ${BASE_DIR}/PYCHOPPER/${SAMPLE_ID}/stats/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.tsv ${BASE_DIR}/U_FASTQ/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.fastq ${BASE_DIR}/PYCHOPPER/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_cdna_r10.4.1_e8.2_400bps_sup@v5.0.0.trimmed.fastq 
+# Defining file paths
+
+INPUT_BAM="${ONT_BAM_DIR}/${SAMPLE_ID}_${FLOWCELL}_${basecalling_model}.bam"
+OUTPUT_FASTQ="${FASTQ_DIR}/${SAMPLE_ID}_${FLOWCELL}_${basecalling_model}.fastq"
+TRIMMED_FASTQ="${PYCHOPPER_DIR}/${SAMPLE_ID}_${FLOWCELL}_${basecalling_model}.trimmed.fastq"
+PYCHOPPER_PDF="${STATS_DIR}/${SAMPLE_ID}_${FLOWCELL}_${basecalling_model}.pdf"
+PYCHOPPER_TSV="${STATS_DIR}/${SAMPLE_ID}_${FLOWCELL}_${basecalling_model}.tsv"
+
+# Debugging output
+echo "Processing BAM file: $INPUT_BAM"
+echo "Output FASTQ: $OUTPUT_FASTQ"
+echo "Trimmed FASTQ: $TRIMMED_FASTQ"
+
+# Converting BAM to FASTQ using samtools
+
+samtools fastq \
+-T* \
+-@ $SLURM_CPUS_PER_TASK \
+-n \
+"${INPUT_BAM}" \
+> "${OUTPUT_FASTQ}" && \
+pychopper \
+-t $SLURM_CPUS_PER_TASK \
+-m phmm \
+-k PCS114 \
+-r "${PYCHOPPER_PDF}" \
+-S "${PYCHOPPER_TSV}" \
+"${OUTPUT_FASTQ}" \
+"${TRIMMED_FASTQ}" 
 
 
 #How to run the script
