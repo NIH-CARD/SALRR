e0a54e4851dc37aba74df13f23a1b6a7350c4d08
cedric-k-n <cedricdixi@gmail.com>
2025-03-14 14:32:23 -0400

    "updated sirv_assembly.sh with structure and readability improvements"

--- sirv_assembly.sh
+++ sirv_assembly.sh
@@ -23,14 +23,19 @@ echo "SAMPLE_ID ${SAMPLE_ID}"
 echo "FLOWCELL ${FLOWCELL}"
 
 # base directory paths
-#BASE_DIR=/data/CARD_AUX/LRS_temp/December_24/ # for example
 BASE_DIR="/data/CARDPB/data/LRS_RNA/data/NGD"
+STR_ASM="${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV"
+ISO_ASM="${BASE_DIR}/ASSEMBLY/ISOQUANT/${SAMPLE_ID}/SIRV"
+MAPPED_DIR="${BASE_DIR}/MAPPED/${SAMPLE_ID}"
+ref_base_dir="/data/CARDPB/data/LRS_RNA/projects/cedrics_nabec/REFERENCES/SIRV"
+REF_GTF="${ref_base_dir}/SIRV_Set4_Norm_Sequences_20210507/SIRV_ERCC_longSIRV_multi-fasta_20210507.gtf"
+CORRECTED_GTF="${ref_base_dir}/SIRV_ERCC_longSIRV_multi-fasta_20210507.corrected.gtf"
+REF_FASTA="${ref_base_dir}/SIRV_ERCC_longSIRV_multi-fasta_20210507.fasta"
+
 
 # make output directories and parent if necessary
 
-mkdir -p ${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV/ #stringtie output directory
-
-mkdir -p ${BASE_DIR}/ASSEMBLY/ISOQUANT/${SAMPLE_ID}/SIRV/ #isoquant output directory
+mkdir -p "${STR_ASM}" "${ISO_ASM}" # stringtie isoquant output directory
 
 # load modules
 module load stringtie/2.2.3
@@ -38,33 +43,38 @@ module load isoquant/3.6.2
 
 # Assembly
 # debugging output with output path for unmapped BAM
-echo "${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf"
-echo "${BASE_DIR}/ASSEMBLY/ISOQUANT/${SAMPLE_ID}/SIRV/"
+echo "${STR_ASM}/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf"
+echo "${ISO_ASM}"
+# Run SIRV Stringtie Assembly
 stringtie \
-${BASE_DIR}/MAPPED/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
--L \
--p $SLURM_CPUS_PER_TASK \
--G /data/CARDPB/data/LRS_RNA/projects/cedrics_nabec/REFERENCES/SIRV/SIRV_Set4_Norm_Sequences_20210507/SIRV_ERCC_longSIRV_multi-fasta_20210507.gtf \
--o ${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf \
-&& \
+    ${MAPPED_DIR}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
+    -L \
+    -p $SLURM_CPUS_PER_TASK \
+    -G ${REF_GTF} \
+    -o ${STR_ASM}/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf || exit 1
+
+# Run SIRV Isoquant Assembly
+
 isoquant.py \
--t 60 \
---reference /data/CARDPB/data/LRS_RNA/projects/cedrics_nabec/REFERENCES/SIRV/SIRV_ERCC_longSIRV_multi-fasta_20210507.fasta \
---genedb /data/CARDPB/data/LRS_RNA/projects/cedrics_nabec/REFERENCES/SIRV/SIRV_ERCC_longSIRV_multi-fasta_20210507.corrected.gtf \
---bam ${BASE_DIR}/MAPPED/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
---data_type nanopore \
---check_canonical \
---sqanti_output \
---prefix ${SAMPLE_ID} \
--o ${BASE_DIR}/ASSEMBLY/ISOQUANT/${SAMPLE_ID}/SIRV/ \
-&& \
+    -t 60 \
+    --reference ${REF_FASTA} \
+    --genedb ${CORRECTED_GTF} \
+    --bam ${MAPPED_DIR}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
+    --data_type nanopore \
+    --check_canonical \
+    --sqanti_output \
+    --prefix ${SAMPLE_ID} \
+    -o ${ISO_ASM} || exit 1
+
+# Run SIRV Stringtie quantification
+
 stringtie \
-${BASE_DIR}/MAPPED/${SAMPLE_ID}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
--e \
--L \
--p $SLURM_CPUS_PER_TASK \
--G ${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf \
--o ${BASE_DIR}/ASSEMBLY/STRINGTIE/${SAMPLE_ID}/SIRV/${SAMPLE_ID}_${FLOWCELL}_sirv_quantified_STR_asm.gtf
+    ${MAPPED_DIR}/${SAMPLE_ID}_${FLOWCELL}_SIRV_mapped_filtered.sorted.bam \
+    -e \
+    -L \
+    -p $SLURM_CPUS_PER_TASK \
+    -G ${STR_ASM}/${SAMPLE_ID}_${FLOWCELL}_sirv_STR_asm.gtf \
+    -o ${STR_ASM}/${SAMPLE_ID}_${FLOWCELL}_sirv_quantified_STR_asm.gtf
 
 
 # How to run the script
