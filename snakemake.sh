8731e538c77411adbb509b848c69809f49167918
Cory Weller <cory.a.weller@gmail.com>
2025-06-03 09:12:23 -0400

    add snakemake example

---
+++ snakemake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH --time 2-0:00:00
+
+
+module purge
+module load snakemake/7.32.4 
+
+# Clone the biowulf snakemake profile
+if [[ ! -d snakemake_profile ]]; then
+    git clone https://github.com/NIH-HPC/snakemake_profile.git
+fi
+
+# Run snakemake
+snakemake --profile snakemake_profile $@
